e30c5677f86c4b04068bbb30b3f5ed0bc522de8b
markhorvath <markhorvath7@gmail.com>
2017-06-23 13:51:04 -0400

    pep8 style fixed, updated readme for reviewer, added output txt file

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-create view views as select path, count(*) as views from log where status = '200 OK' and path != '/' group by path order by views DESC;
+# Logs Analysis Project for Udacity FSND
+#### A project using PostgreSQL and Python to query a database for a hypothetical newspaper website.  Program outputs the most popular articles and authors according to the number of views they receive, as well as any dates where more than 1% of HTTP requests led to errors.  IMPORTANT: Program requires vagrant and virtual machine which can be found here: https://github.com/markhorvath/FSND-Virtual-Machine/tree/master/vagrant and the required database "news" here: https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip
 
-create view titleview as select author, views from articles join views on views.path like '%' || articles.slug;
+## TO THE REVIEWER
+#### Once inside psql, please create the following views by running them in the command line:
 
-create view logview as select date(time), count(log.time), sum(case when log.status = '404 NOT FOUND' then 1 else 0 end) Errors from log group by date;
+1. create view views as select path, count(*) as views from log where status = '200 OK' and path != '/' group by path order by views DESC;
 
-create view geterrors as select date, round(((errors * 100.0) / count), 2) as percent from logview;
+2. create view titleview as select author, views from articles join views on views.path like '%' || articles.slug;
+
+3. create view logview as select date(time), count(log.time), sum(case when log.status = '404 NOT FOUND' then 1 else 0 end) Errors from log group by date;
+
+4. create view geterrors as select date, round(((errors * 100.0) / count), 2) as percent from logview;
+
+#### After these are created, you should be able to simply run "python logs-analysis.py" and it should output the expected data.
--- logs-analysis.py
+++ logs-analysis.py
@@ -10,36 +10,51 @@ import psycopg2
 
 DBNAME = "news"
 
+
 def get_top_articles():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("select articles.title, views.views from articles join views on views.path like '%' || articles.slug limit 3;")
+    c.execute("""select articles.title, views.views 
+                  from articles 
+                  join views on views.path like '%' || articles.slug limit 3;
+              """)
     results = c.fetchall()
     print('\n' + 'The top 3 most viewed articles are:')
     for (title, views) in results:
         print('"' + title + '" - ' + str(views) + ' views')
     db.close()
-    
+
+
 def get_top_authors():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("select authors.name, sum(views) from authors, titleview where authors.id = titleview.author group by authors.name order by sum DESC;")
-    results =  c.fetchall()
+    c.execute("""select authors.name, sum(views) 
+                 from authors, titleview 
+                 where authors.id = titleview.author 
+                 group by authors.name 
+                 order by sum DESC;
+              """)
+    results = c.fetchall()
     print('\n' + 'The most popular authors of all time are:')
     for (name, sum) in results:
         print(name + ' - ' + str(sum) + ' views')
     db.close()
-    
+
+
 def get_errors():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("select to_char(date, 'fmMonth DD, YYYY') as day, percent from geterrors where percent > 1.0;")
+    c.execute("""select to_char(date, 'fmMonth DD, YYYY') as day, percent 
+                 from geterrors 
+                 where percent > 1.0;
+              """)
     results = c.fetchall()
     print('\n' + 'Days where more than 1% of requests led to errors:')
     for (day, percent) in results:
         print(str(day) + ' - ' + str(percent) + '% errors')
     db.close()
-    
+
+
 get_top_articles()
 get_top_authors()
 get_errors()
